34c71c9ffbead91321f4909ba1673be3f3275b40
caioCodigo <caio_pfl@hotmail.com>
2019-02-26 19:41:46 -0300

    começo

---
+++ application/views/dashboard/home.php
@@ -0,0 +1,133 @@
+<div>
+    <div class="home">
+
+        <div class="section">
+            <div class="section__texts">
+                <p class="title-section">Destaques</p>
+                <p class="desc-section">Aqui você pode ver as publicações mais comentadas do momento.</p>
+            </div>
+            <div class="section__posts">
+                <div class="principal">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">O medo é vencivel !</p>
+                            <div class="description">
+                                <p class="descricao-principal">Descubra como podemos vencer nossos medos.</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+
+                        </div>
+                        <div class="image-content">
+                            <img src="http://www.spckoralawella.com/images/home_footer/icon/bible-banner.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="secundario">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">Oque acontece com a nossa geração ?</p>
+                            <div class="description">
+                                <p class="descricao-principal">Oque causou a corrupção do homem nos tempos atuais.</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+                            
+                        </div>
+                        <div class="image-content">
+                            <img src="https://staticr1.blastingcdn.com/media/photogallery/2016/11/25/660x290/b_620x273/pastor-genival-bento-foto-divulgacao_999903.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="terceiro">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">O sucesso de um cristão !</p>
+                            <div class="description">
+                                <p class="descricao-principal">Você sabe oque a biblia diz sobre ser "bem-sucedido".</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+                            
+                        </div>
+                        <div class="image-content">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIa8Ya4g57dUyI9tQa-7AJHzMY3LTuYrFkTCojyBSo4taOs3BT6Q" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section__more">
+                <div class="text">
+                    <p>Ver todos</p>
+                </div>
+                <div class="icon">
+                    <img src="https://cdn1.iconfinder.com/data/icons/mixed-17/16/icon_right_rounded-512.png" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="section__texts">
+                <p class="title-section">Debates</p>
+                <p class="desc-section">Aqui você pode debater os assuntos mais falados do momento.</p>
+            </div>
+            <div class="section__posts">
+                <div class="principal">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">O medo é vencivel !</p>
+                            <div class="description">
+                                <p class="descricao-principal">Descubra como podemos vencer nossos medos.</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+
+                        </div>
+                        <div class="image-content">
+                            <img src="http://www.spckoralawella.com/images/home_footer/icon/bible-banner.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="secundario">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">Oque acontece com a nossa geração ?</p>
+                            <div class="description">
+                                <p class="descricao-principal">Oque causou a corrupção do homem nos tempos atuais.</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+                            
+                        </div>
+                        <div class="image-content">
+                            <img src="https://staticr1.blastingcdn.com/media/photogallery/2016/11/25/660x290/b_620x273/pastor-genival-bento-foto-divulgacao_999903.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="terceiro">
+                    <div class="principal-container">
+                        <div class="text-content">
+                            <p class="title-principal">O sucesso de um cristão !</p>
+                            <div class="description">
+                                <p class="descricao-principal">Você sabe oque a biblia diz sobre ser "bem-sucedido".</p><span class="autor">~ Por Pastor ulisses</span>
+                            </div>
+                        </div>
+                        <div class="overlay-content">
+                            
+                        </div>
+                        <div class="image-content">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIa8Ya4g57dUyI9tQa-7AJHzMY3LTuYrFkTCojyBSo4taOs3BT6Q" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section__more">
+                <div class="text">
+                    <p>Ver todos</p>
+                </div>
+                <div class="icon">
+                    <img src="https://cdn1.iconfinder.com/data/icons/mixed-17/16/icon_right_rounded-512.png" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
